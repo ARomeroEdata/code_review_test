b26d7abeb5edbed98df92a665f072b4be9d3e266
Abraham Romero <abraham.romero@edataconsulting.de>
2018-02-21 15:11:35 +0000

    First commit with the basic structure of the project.

---
+++ restapi/src/main/java/es/edataconsulting/restapi/ServletInitializer.java
@@ -0,0 +1,13 @@
+package es.edataconsulting.restapi;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(RestapiApplication.class);
+	}
+
+}
